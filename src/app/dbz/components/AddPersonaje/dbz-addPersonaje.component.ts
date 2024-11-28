6f0e6773c6fced5c26b7c897a3fc3bd67562ba6b
Karim <layne_2002@icloud.com>
2024-11-28 20:21:48 +0100

    @output

--- src/app/dbz/components/AddPersonaje/dbz-addPersonaje.component.ts
+++ src/app/dbz/components/AddPersonaje/dbz-addPersonaje.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Personaje } from '../../interfaces/personaje.interface';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'dbz-add-personaje',
   templateUrl: 'dbz-addpersonaje.component.html',
-  standalone:true
+  standalone: true,
+  imports: [FormsModule, CommonModule],
 })
+export class AddPersonajeComponent {
+  @Output()
+  public onNewPersonaje: EventEmitter<Personaje> = new EventEmitter();
 
-export class dbzAddPersonaje{
-
+  public personaje: Personaje = {
+    nombre: '',
+    fuerza: 0,
+  };
+  public addPersonaje(): void {
+    console.log("addPersonaje")
+    console.log(this.personaje);
+    if(this.personaje.nombre.length==0) return;
+    this.onNewPersonaje.emit(this.personaje);
+    // Inicializamos valores
+    this.personaje.nombre = '';
+    this.personaje.fuerza = 0;
+  }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { dbzListaComponent } from '../components/Lista/dbz-lista.component';
-import { dbzAddPersonaje } from '../components/AddPersonaje/dbz-addPersonaje.component';
+import { AddPersonajeComponent } from '../components/AddPersonaje/dbz-addPersonaje.component';
 import { Personaje } from '../interfaces/personaje.interface';
+import { log } from 'node:console';
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html',
   standalone: true,
-  imports:[dbzListaComponent,dbzAddPersonaje]
+  imports: [dbzListaComponent, AddPersonajeComponent],
 })
-
 export class MainPageComponent {
-public personajes: Personaje[]=[{
-  nombre: 'Krillin',
-  fuerza: 500
-},{
-  nombre: 'Goku',
-  fuerza:10000
-}
-];
+  public personajes: Personaje[] = [
+    {
+      nombre: 'Krillin',
+      fuerza: 500,
+    },
+    {
+      nombre: 'Goku',
+      fuerza: 10000,
+    },
+    {
+      nombre: 'Vegeta',
+      fuerza: 2000,
+    },
+  ];
+  public onNewPersonaje(personaje:Personaje):void{
+    console.log("Main page")
+    console.log(personaje);
+  }
 }
