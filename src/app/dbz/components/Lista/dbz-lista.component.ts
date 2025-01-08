cdce0f2c9ded9b6adfdf76180cb065fda43dee7a
Karim <layne_2002@icloud.com>
2025-01-08 17:16:00 +0100

    subimos dist como docs

--- src/app/dbz/components/Lista/dbz-lista.component.ts
+++ src/app/dbz/components/Lista/dbz-lista.component.ts
@@ -1,16 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Personaje } from '../../interfaces/personaje.interface';
 @Component({
   selector: 'dbz-lista',
   templateUrl: 'dbz-lista.component.html',
-  standalone:true,
-  imports:[CommonModule]
+  standalone: true,
+  imports: [CommonModule],
 })
 export class dbzListaComponent {
   @Input('miLista')
-  public listaPersonajes: Personaje[]=[{
-    nombre: 'Trunks',
-    fuerza:7000
-  }];
+  public listaPersonajes: Personaje[] = [];
+
+  @Output()
+  public onDeletePersonaje: EventEmitter<string> = new EventEmitter();
+
+  public eliminarPersonaje(id: string | undefined) {
+    this.onDeletePersonaje.emit(id);
+  }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { dbzListaComponent } from '../components/Lista/dbz-lista.component';
 import { AddPersonajeComponent } from '../components/AddPersonaje/dbz-addPersonaje.component';
+import { DbzService } from '../services/dbz.service';
 import { Personaje } from '../interfaces/personaje.interface';
-import { log } from 'node:console';
+
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html',
@@ -10,22 +11,15 @@ import { log } from 'node:console';
   imports: [dbzListaComponent, AddPersonajeComponent],
 })
 export class MainPageComponent {
-  public personajes: Personaje[] = [
-    {
-      nombre: 'Krillin',
-      fuerza: 500,
-    },
-    {
-      nombre: 'Goku',
-      fuerza: 10000,
-    },
-    {
-      nombre: 'Vegeta',
-      fuerza: 2000,
-    },
-  ];
-  public onNewPersonaje(personaje:Personaje):void{
-    console.log("Main page")
-    console.log(personaje);
+  constructor(private dbzService: DbzService) {}
+
+  get personajes(): Personaje[] {
+    return this.dbzService.personajes;
+  }
+  onNewPersonaje(personaje: Personaje): void {
+    this.dbzService.addPersonaje(personaje);
+  }
+  onDeletePersonaje(id: string): void {
+    this.dbzService.deletePersonaje(id);
   }
 }
